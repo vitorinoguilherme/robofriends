4b1e6352a3cca8c38259b57739d4f6a416018923
guilherme vitorino <guilhermevitorino.dev@gmail.com>
2022-02-02 14:24:43 -0300

    feat: add new endpoint to load robots

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
-
+//import { robots } from './robots';
+import './App.css';
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: '',
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ robots: users }))
   }
 
   onSearchChange = (event) => {
@@ -23,15 +29,17 @@ class App extends Component {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     })
 
-    console.log(filteredRobots);
-
-    return (
-      <div className="tc">
-        <h1>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots={ filteredRobots }/>
-      </div>
-    )
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <CardList robots={ filteredRobots }/>
+        </div>
+      )
+    }
   }
 }
 
