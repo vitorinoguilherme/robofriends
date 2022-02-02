cda11e62ed2f8c6479df358d26d0e401be7239cd
guilherme vitorino <guilhermevitorino.dev@gmail.com>
2022-02-02 15:20:51 -0300

    refactor: change folder structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,10 @@
 import React, { Component} from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-//import { robots } from './robots';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+
 import './App.css';
+
 class App extends Component {
   constructor() {
     super()
@@ -20,26 +22,27 @@ class App extends Component {
 
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value });
-
-    
   }
 
   render() {
-    const filteredRobots = this.state.robots.filter(robot => {
-      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    const { robots, searchfield } = this.state;
+
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
 
-    if (this.state.robots.length === 0) {
-      return <h1>Loading</h1>
-    } else {
-      return (
+    return !robots.length ? 
+      <h1>Loading</h1> :
+      (
         <div className="tc">
           <h1 className="f1">RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange}/>
-          <CardList robots={ filteredRobots }/>
+          <Scroll>
+            <CardList robots={ filteredRobots }/>
+          </Scroll>
         </div>
       )
-    }
+    
   }
 }
 
