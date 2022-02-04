84fa599af383f12c37e53cf0a74d97abfaf82d0f
guilherme vitorino <guilhermevitorino.dev@gmail.com>
2022-02-04 17:04:19 -0300

    feat: implement error boundary into react app

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,8 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 
+import ErrorBoundry from '../components/ErrorBoundry';
+
 import './App.css';
 
 class App extends Component {
@@ -38,7 +40,9 @@ class App extends Component {
           <h1 className="f1">RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange}/>
           <Scroll>
-            <CardList robots={ filteredRobots }/>
+            <ErrorBoundry>
+              <CardList robots={ filteredRobots }/>
+            </ErrorBoundry>
           </Scroll>
         </div>
       )
