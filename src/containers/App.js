bdf8d4ce1051fcd6ad27843aba52c0566386244b
guilherme vitorino <guilhermevitorino.dev@gmail.com>
2022-02-05 18:11:17 -0300

    refactor: change state management to redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
@@ -7,27 +9,34 @@ import ErrorBoundry from '../components/ErrorBoundry';
 
 import './App.css';
 
-function App() {
-  const [robots, setRobots] = useState([]);
-  const [searchfield, setSearchfield] = useState('');
-  
+import { setSearchField, requestRobots } from '../actions';
+
+const mapStateToProps = (state) => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return { 
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
+  }
+}
+
+function App({ searchField, onSearchChange, robots, isPending, onRequestRobots }) {  
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => 
-      response.json()
-    )
-    .then(users => setRobots(users))
+    onRequestRobots()
   }, [])
   
-  const onSearchChange = (event) => {
-    setSearchfield(event.target.value);
-  }
-    
   const filteredRobots = robots.filter(robot => {
-    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    return robot.name.toLowerCase().includes(searchField.toLowerCase());
   })
   
-  return !robots.length ? 
+  return isPending ? 
     <h1>Loading</h1> :
     (
       <div className="tc">
@@ -42,4 +51,4 @@ function App() {
     ) 
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux'; 
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import 'tachyons'
 
+import App from './containers/App';
+import reportWebVitals from './reportWebVitals';
+import { searchRobots, requestRobots } from './reducers';
+import './index.css';
+
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
